40abb1e133b17d0a9cc1ef8c62e2f92c3be5f94a
JENkt4k <james.e.nelson@gmail.com>
2025-08-21 01:28:27 -0500

    move icons

--- vite.config.ts
+++ vite.config.ts
@@ -27,12 +27,12 @@ export default defineConfig({
         orientation: 'portrait',
         icons: [
           {
-            src: 'pwa-icon-192.png',
+            src: './icons/pwa-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-icon-512.png',
+            src: './icons/pwa-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           }
